b042ee07b9f330f241ac5967c987f437605496e3
Manuel Prince <manuelsdesk0029@gmail.com>
2025-10-30 23:54:21 +0100

    Assignment one

---
+++ assignment.py
@@ -0,0 +1,26 @@
+
+#Addition
+def add(a, b):
+    return a + b
+
+#Subtraction
+def subtract(a, b):
+    return f"The Result of the Subtraction is {a - b}"
+
+#Multiplication
+def multiply(a, b):
+    return f"The result of the Multiplication is {a * b}"
+
+#Division
+def divide(a, b):
+    return f"The result of the division is {a / b}"
+
+#Modulus
+def get_modulus(a, b):
+    modulus = a % b
+    return f"The modulus of the number is {modulus}"
+
+#Modulus
+print(get_modulus(7, 3))
+#Division
+print(divide(5, 7))
